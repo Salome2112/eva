69adf6a3905908049247193917bb2b783afe4d03
Salome2112 <146013347+Salome2112@users.noreply.github.com>
2025-07-29 13:05:05 -0500

    1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  cursos = [
+    {
+      nombre: 'I+DEC = Investigación + Desarrollo, Educación y Competencias',
+      categoria: 'PLANTILLAS',
+      imagen: 'C:\Users\USER\eva\src\assets\images\Captura de pantalla 2025-07-29 122807.png'
+    },
+    {
+      nombre: 'Equidad de Genero',
+      categoria: 'PLANTILLAS',
+      imagen: 'C:\Users\USER\eva\src\assets\images\Captura de pantalla 2025-07-29 123017.png'
+    },
+    {
+      nombre: 'Aula Plantilla',
+      categoria: 'PLANTILLAS',
+      imagen: 'C:\Users\USER\eva\src\assets\images\Captura de pantalla 2025-07-29 123030.png'
+    },
+    {
+      nombre: 'TEORÍA DEL DISEÑO',
+      categoria: '1ER NIVEL [27-1-1-12-1]',
+      imagen: 'src\assets\images\Captura de pantalla 2025-07-29 123041.png'
+    },
+    {
+      nombre: 'RETOQUE DIGITAL Y VECTORES - MATUTINO',
+      categoria: '1ER NIVEL [27-1-1-12-1]',
+      imagen: 'src\assets\images\Captura de pantalla 2025-07-29 123047.png'
+    }
+  ];
+
   constructor() {}
 
 }
